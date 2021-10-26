7eec9f96128e68950e410d733bbe6198369252bc
Pauline Bertrand <bertrand.pauline@gmail.com>
2021-10-26 11:21:12 +0200

    began to work on borrow buttons on all-books

--- public/scripts/all-books.js
+++ public/scripts/all-books.js
@@ -1,8 +1,15 @@
-function renderAddToWishlistButton() {
-    // Iteration 1: set the visibility of `<section class="green-pepper">`
-    document.querySelectorAll('.add-wishlist').forEach((addButton) => {
+// function renderAddToWishlistButton() {
+//     document.querySelectorAll('.add-wishlist').forEach((addButton) => {
     
-        addButton.style.visibility = 'visible';
+//         addButton.style.visibility = 'visible';
       
-    });
-  }
+//     });
+//   }
+console.log("im in public/scripts")
+const borrowButtons = document.querySelectorAll(".borrow-button")
+borrowButtons.forEach((borrowButton) => {
+    borrowButton.addEventListener('click', () => {
+        console.log("yo")
+        borrowButton.classList.remove("active")
+    })
+})
