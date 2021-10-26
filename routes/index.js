4c527f4010fb5813db84060026b70fa8cf28663c
Marie Popoff <mariepopoff01@gmail.com>
2021-10-26 16:15:17 +0200

    changed helpers

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bookModel = require("./../models/bookModel")
-const userModel = require("./../models/userModel")
-const borrowingModel = require ("./../models/borrowingModel")
+const bookModel = require("./../models/bookModel");
+const userModel = require("./../models/userModel");
+const borrowingModel = require("./../models/borrowingModel");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 router.get("/all-books", (req, res, next) => {
-  bookModel.find( { owner : { $ne: req.session.currentUser?._id,} })
-  .then((books) => res.render("all-books/all-books.hbs", { books }))
-  .catch((error) => console.error(error))
-})
+  const databaseRequests = [
+    bookModel.find({ owner: { $ne: req.session.currentUser._id } }),
+    userModel.findById(req.session.currentUser._id).populate("wishlist")
+  ]
+Promise.all(databaseRequests)
+    .then((responses) => res.render("all-books/all-books.hbs", { books: responses[0], user: responses[1] }))
+    .catch((error) => console.error(error));
+});
 
 module.exports = router;
