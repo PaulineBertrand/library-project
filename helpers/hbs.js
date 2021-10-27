aecdb6952fa46cdc28d0b75bbc03819bf69174c2
Marie Popoff <mariepopoff01@gmail.com>
2021-10-27 10:34:03 +0200

    on the edit page, the genre of the book is selected, and changed the messages in the dashboard pages depending on the number of books lended, borrowed, wishlist

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -17,3 +17,24 @@ hbs.registerHelper("isInWishlist", function (user, options) {
 hbs.registerHelper("ifEquals", function (x, y, thingToDisplay) {
   if (x === y) return thingToDisplay
 });
+
+hbs.registerHelper("isEqual", function (nbAllBooks, nbLendedBooks) {
+  if (nbAllBooks === 1) 
+  return `<p>You have ${nbAllBooks} book, among which ${nbLendedBooks} currently lended.</p>`;
+  if (nbAllBooks > 1)
+  return `<p>You have ${nbAllBooks} books, among which ${nbLendedBooks} currently lended.</p>`
+});
+
+hbs.registerHelper("isEqualWishlist", function (nbWishlistBooks) {
+  if (nbWishlistBooks === 1) 
+  return `<p>You have ${nbWishlistBooks} book in your wishlist.</p>`;
+  if (nbWishlistBooks > 1)
+  return `   <p>You have ${nbWishlistBooks} books in your wishlist.</p>`
+});
+
+hbs.registerHelper("isEqualBorrowed", function (nbBorrowedBooks) {
+  if (nbBorrowedBooks === 1) 
+  return `<p>You are borrowing ${nbBorrowedBooks} book.</p>`;
+  if (nbBorrowedBooks > 1)
+  return `<p>You are borrowing ${nbBorrowedBooks} books.</p>`
+});
