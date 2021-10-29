1cea7d6b8523d179d42a282d3745cf9c390aec46
Pauline Bertrand <bertrand.pauline@gmail.com>
2021-10-29 12:09:23 +0200

    replaceAll does not work on heroku, used replace again

--- middlewares/bookCovers.js
+++ middlewares/bookCovers.js
@@ -14,7 +14,7 @@ module.exports = function findCoverImage(req, res, next) {
     // we get the wrong book. The first API, search openlibrary, returns a list of books and we take the first one. In this info, what 
     // we are interested in is the reference number for a cover picture.
 
-    const coverIdUrl = '/search.json?title=' + removeAccents(req.body.title).toLowerCase().replaceAll(" ", "+") + '&author=' + removeAccents(req.body.author).toLowerCase().replaceAll(" ", "+");
+    const coverIdUrl = '/search.json?title=' + removeAccents(req.body.title).toLowerCase().replace(" ", "+", "g") + '&author=' + removeAccents(req.body.author).toLowerCase().replace(" ", "+", "g");
     console.log(coverIdUrl)
     const api = axios.create({
         baseURL: 'https://openlibrary.org'
